7c6b06a5d77a1caba38c1c13a5b17edea84268f2
이은상 <kyrsang0801@inbrein.com>
2024-07-03 10:02:42 +0900

    Chore: unused

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -1,40 +1,40 @@
-import React, { useEffect, useState } from "react";
-
-const BackToTopButton = () => {
-    const [showTopBtn, setShowTopBtn] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 200) {
-                setShowTopBtn(true);
-            } else {
-                setShowTopBtn(false);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
-    };
-
-    return (
-        <>
-            {showTopBtn && (
-                <div onClick={scrollToTop} className="btn badge bg-secondary-subtle text-secondary rounded-pill fw-lighter">
-                    back to top
-                </div>
-            )}
-        </>
-    );
-};
-
-export default BackToTopButton;
+// import React, { useEffect, useState } from "react";
+//
+// const BackToTopButton = () => {
+//     const [showTopBtn, setShowTopBtn] = useState(false);
+//
+//     useEffect(() => {
+//         const handleScroll = () => {
+//             if (window.scrollY > 200) {
+//                 setShowTopBtn(true);
+//             } else {
+//                 setShowTopBtn(false);
+//             }
+//         };
+//
+//         window.addEventListener("scroll", handleScroll);
+//
+//         return () => {
+//             window.removeEventListener("scroll", handleScroll);
+//         };
+//     }, []);
+//
+//     const scrollToTop = () => {
+//         window.scrollTo({
+//             top: 0,
+//             behavior: "smooth",
+//         });
+//     };
+//
+//     return (
+//         <>
+//             {showTopBtn && (
+//                 <div onClick={scrollToTop} className="btn badge bg-secondary-subtle text-secondary rounded-pill fw-lighter">
+//                     back to top
+//                 </div>
+//             )}
+//         </>
+//     );
+// };
+//
+// export default BackToTopButton;
